3395337f37ded5efdc643d72f3f1e391094417b5
ztsmith25 <ztsmith@me.com>
2017-09-26 14:53:34 -0700

    create and add App component to index.js

--- projects/proj_0/ReduxSimpleStarter-master/src/index.js
+++ projects/proj_0/ReduxSimpleStarter-master/src/index.js
@@ -1,4 +1,15 @@
-// 1. create a new component that should produce some HTML
+import React from 'react'; // for creating new components
+import ReactDOM from 'react-dom'; // for adding component to DOM
 
+// 1. create a new component that should produce some HTML
+const App = () => { // const is ES6 syntax for declaring a constant
+  return <div>Hi!</div>; // JSX dialect of JS that allows HTMLish inside
+}  // App is a class, not an instance!!!
+   // this function is like a factory for producing instances to get rendered
+   // => is ES6 for function keyword before the ()
 
 // 2. need to take this component's generated HTML and put it in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
+// first arg is JSX instance, 2nd arg is existing HTML node to render to
+// putting a JSX tag around class turns it to instance
+// when wrap a class (like App) with JSX tags (<App />) it becomes an instance
